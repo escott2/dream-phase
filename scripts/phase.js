4e9f16a1c805849b8de2f633e5dcb02311de5788
escott2 <etriplett14@gmail.com>
2025-03-19 15:14:21 -0500

    refactor phase javascript

--- scripts/phase.js
+++ scripts/phase.js
@@ -5,146 +5,167 @@ import {
   handleEditListButtonClick,
   handleEditModeButtonClick,
 } from "./utils.js";
+import { initializeTheme } from "./theme.js";
 import { createCloudSVG } from "./cloudSVG.js";
 
-const addDreamForm = document.querySelector("#js-add-dream-form");
-const dreamPhaseList = document.querySelector(".js-dream-phase-list");
-const editModeButton = document.querySelector(".js-edit-mode-button");
-const editListButton = document.querySelector(".js-edit-list-button");
-const phaseNameHeading = document.querySelector(".js-phase-name");
-const breadcrumbs = document.querySelector(".js-breadcrumbs");
-const pageTitleContainer = document.querySelector(".js-page-title-container");
-const appContainer = document.querySelector(".js-application-container");
+function createDreamPhasePageManager(
+  dreamPhaseList,
+  addDreamForm,
+  phaseNameHeading,
+  breadcrumbs,
+  pageTitleContainer
+) {
+  const params = new URLSearchParams(window.location.search);
+  let phaseId = params.get("id");
+  let dreamPhaseData;
+  let phaseData;
+  let dreams;
 
-const params = new URLSearchParams(window.location.search);
-let phaseId = params.get("id");
-
-let dreamPhaseData;
-let phaseData;
-let dreams;
-
-function saveDreams(dreamPhaseData, phaseData, dreams) {
-  const updatedPhaseData = {
-    ...phaseData,
-    dreams: dreams,
-  };
-
-  const index = dreamPhaseData.findIndex(
-    (element) => element.id === parseInt(phaseId)
-  );
-
-  if (index !== -1) {
-    const newDreamPhaseData = [...dreamPhaseData];
-    newDreamPhaseData[index] = updatedPhaseData;
-    saveData(newDreamPhaseData, "dreamPhaseData");
-  }
-}
-
-function renderPhaseNameHeading(phaseName) {
-  phaseNameHeading.textContent = phaseName;
-}
-
-function renderPhaseNameBreadcrumbs(phaseName) {
-  const newText = document.createTextNode(phaseName);
-  breadcrumbs.appendChild(newText);
-}
-
-function renderCloudSVG(color) {
-  const cloudSVG = createCloudSVG();
-  pageTitleContainer.appendChild(cloudSVG);
-}
-
-function renderItem(item) {
-  const newItem = document.createElement("li");
-  newItem.dataset.id = item.id;
-  newItem.classList.add("dream-item");
-  const checkboxInput = document.createElement("input");
-  checkboxInput.setAttribute("type", "checkbox");
-  checkboxInput.setAttribute("id", `${item.id}`);
-  checkboxInput.checked = item.isChecked;
-  checkboxInput.classList.add("js-dream-checkbox");
-  const checkboxLabel = document.createElement("label");
-  checkboxLabel.classList.add("dream-name");
-  checkboxLabel.textContent = item.value;
-  checkboxLabel.setAttribute("for", `${item.id}`);
-  const deleteButton = createDeleteButton();
-  newItem.append(checkboxInput, checkboxLabel, deleteButton);
-  dreamPhaseList.appendChild(newItem);
-}
-
-function renderList(dreams) {
-  dreamPhaseList.replaceChildren();
-  dreams.forEach((item) => {
-    renderItem(item);
-  });
-}
-
-function removeItem(itemId) {
-  const newDreams = dreams.filter((dream) => {
-    return dream.id.toString() !== itemId;
-  });
-  dreams = newDreams;
-  saveDreams(dreamPhaseData, phaseData, dreams);
-  renderList(dreams);
-}
-
-if (phaseId) {
-  dreamPhaseData = loadData("dreamPhaseData");
-  phaseData = dreamPhaseData.find(
-    (element) => element.id === parseInt(phaseId)
-  );
-
-  if (phaseData) {
-    dreams = phaseData.dreams;
-    renderList(dreams);
-    renderCloudSVG("#7891c3");
-    renderPhaseNameHeading(phaseData.name);
-    renderPhaseNameBreadcrumbs(phaseData.name);
-  } else {
-    console.log("error loading phase data");
-  }
-
-  addDreamForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const textInput = addDreamForm.elements["dream-name"];
-    const newItem = {
-      value: textInput.value,
-      id: Date.now(),
-      isChecked: false,
+  function saveDreams() {
+    const updatedPhaseData = {
+      ...phaseData,
+      dreams: dreams,
     };
-    dreams.push(newItem);
 
-    saveDreams(dreamPhaseData, phaseData, dreams);
+    const index = dreamPhaseData.findIndex(
+      (element) => element.id === parseInt(phaseId)
+    );
 
-    renderItem(newItem);
-    textInput.value = "";
-  });
-
-  dreamPhaseList.addEventListener("click", (e) => {
-    if (e.target.classList.contains("js-delete-button")) {
-      const itemId = e.target.parentNode.dataset.id;
-      removeItem(itemId);
+    if (index !== -1) {
+      const newDreamPhaseData = [...dreamPhaseData];
+      newDreamPhaseData[index] = updatedPhaseData;
+      saveData(newDreamPhaseData, "dreamPhaseData");
     }
+  }
 
-    if (e.target.classList.contains("js-dream-checkbox")) {
-      const dreamCheckbox = e.target;
-      const itemId = dreamCheckbox.parentNode.dataset.id;
-      const targetDreamIndex = dreams.findIndex(
-        (dream) => dream.id.toString() === itemId
+  function renderPhaseNameHeading(phaseName) {
+    phaseNameHeading.textContent = phaseName;
+  }
+
+  function renderPhaseNameBreadcrumbs(phaseName) {
+    const newText = document.createTextNode(phaseName);
+    breadcrumbs.appendChild(newText);
+  }
+
+  function renderCloudSVG() {
+    const cloudSVG = createCloudSVG();
+    pageTitleContainer.appendChild(cloudSVG);
+  }
+
+  function renderItem(item) {
+    const newItem = document.createElement("li");
+    newItem.dataset.id = item.id;
+    newItem.classList.add("dream-item");
+    const checkboxInput = document.createElement("input");
+    checkboxInput.setAttribute("type", "checkbox");
+    checkboxInput.setAttribute("id", `${item.id}`);
+    checkboxInput.checked = item.isChecked;
+    checkboxInput.classList.add("js-dream-checkbox");
+    const checkboxLabel = document.createElement("label");
+    checkboxLabel.classList.add("dream-name");
+    checkboxLabel.textContent = item.value;
+    checkboxLabel.setAttribute("for", `${item.id}`);
+    const deleteButton = createDeleteButton();
+    newItem.append(checkboxInput, checkboxLabel, deleteButton);
+    dreamPhaseList.appendChild(newItem);
+  }
+
+  function renderList() {
+    dreamPhaseList.replaceChildren();
+    dreams.forEach((item) => {
+      renderItem(item);
+    });
+  }
+
+  function removeItem(itemId) {
+    dreams = dreams.filter((dream) => dream.id.toString() !== itemId);
+    saveDreams();
+    renderList();
+  }
+
+  function initialize() {
+    if (phaseId) {
+      dreamPhaseData = loadData("dreamPhaseData");
+      phaseData = dreamPhaseData.find(
+        (element) => element.id === parseInt(phaseId)
       );
-      const updatedDream = { ...dreams[targetDreamIndex] };
-      updatedDream.isChecked = dreamCheckbox.checked;
-      dreams[targetDreamIndex] = updatedDream;
 
-      saveDreams(dreamPhaseData, phaseData, dreams);
+      if (phaseData) {
+        dreams = phaseData.dreams;
+        renderList();
+        renderCloudSVG();
+        renderPhaseNameHeading(phaseData.name);
+        renderPhaseNameBreadcrumbs(phaseData.name);
+      } else {
+        console.log("error loading phase data");
+      }
+
+      addDreamForm.addEventListener("submit", (e) => {
+        e.preventDefault();
+        const textInput = addDreamForm.elements["dream-name"];
+        const newItem = {
+          value: textInput.value,
+          id: Date.now(),
+          isChecked: false,
+        };
+        dreams.push(newItem);
+        saveDreams();
+        renderItem(newItem);
+        textInput.value = "";
+      });
+
+      dreamPhaseList.addEventListener("click", (e) => {
+        if (e.target.classList.contains("js-delete-button")) {
+          const itemId = e.target.parentNode.dataset.id;
+          removeItem(itemId);
+        }
+
+        if (e.target.classList.contains("js-dream-checkbox")) {
+          const dreamCheckbox = e.target;
+          const itemId = dreamCheckbox.parentNode.dataset.id;
+          const targetDreamIndex = dreams.findIndex(
+            (dream) => dream.id.toString() === itemId
+          );
+          const updatedDream = { ...dreams[targetDreamIndex] };
+          updatedDream.isChecked = dreamCheckbox.checked;
+          dreams[targetDreamIndex] = updatedDream;
+          saveDreams();
+        }
+      });
+    } else {
+      console.log("error: No phase id passed");
     }
-  });
+  }
+
+  return {
+    initialize,
+  };
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const addDreamForm = document.querySelector("#js-add-dream-form");
+  const dreamPhaseList = document.querySelector(".js-dream-phase-list");
+  const editModeButton = document.querySelector(".js-edit-mode-button");
+  const editListButton = document.querySelector(".js-edit-list-button");
+  const phaseNameHeading = document.querySelector(".js-phase-name");
+  const breadcrumbs = document.querySelector(".js-breadcrumbs");
+  const pageTitleContainer = document.querySelector(".js-page-title-container");
+
+  initializeTheme();
+
+  const dreamPhasePageManager = createDreamPhasePageManager(
+    dreamPhaseList,
+    addDreamForm,
+    phaseNameHeading,
+    breadcrumbs,
+    pageTitleContainer
+  );
+
+  dreamPhasePageManager.initialize();
 
   editModeButton.addEventListener("click", () =>
     handleEditModeButtonClick(editListButton)
   );
 
   editListButton.addEventListener("click", handleEditListButtonClick);
-} else {
-  console.log("error: No phase id passed");
-}
+});
